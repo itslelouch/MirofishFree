9f9fa2bd9420812ec6955ce375c7ec8cac47882e
HC <83952851+itslelouch@users.noreply.github.com>
2026-03-30 23:21:03 +0530

    Change navbar brand from 'OFFLINE' to 'FREE'

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home-container">
     <!-- Top Navigation Bar -->
     <nav class="navbar" :style="s.navbar">
-      <div class="nav-brand" :style="s.navBrand">MIROFISH OFFLINE</div>
+      <div class="nav-brand" :style="s.navBrand">MIROFISH FREE</div>
       <div class="nav-links" :style="s.navLinks">
         <a href="https://github.com/itslelouch/MirofishFree" target="_blank" class="github-link" :style="s.githubLink">
           Visit our Github <span>↗</span>
